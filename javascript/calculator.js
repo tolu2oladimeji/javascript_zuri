0f8b5d0e996320c2538e38eee9149c6a22b52648
tolu2oladimeji <105168669+tolu2oladimeji@users.noreply.github.com>
2022-06-22 22:17:57 +0100

    Update calculator.js

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -1,5 +1,26 @@
+//This programs is a simple calculator that can perform basic
+//arithmetic functions such as addition, subtraction
+//multiplication and division
+
+
+//declare the variables
+
 let addition = 0;
 let subtraction = 0;
 let division = 1;
 let multiplication = 1;
-console.log(addition, subtraction, division, multiplication);
+
+//get input from the user
+let number1 = parseFloat(prompt("Enter first number"));
+let number2 = parseFloat(prompt("Enter second number"));
+
+//the computation process
+addition = number1 + number2;
+subtraction= number1 - number2;
+division = number1/number2;
+multiplication = number1*number2;
+
+console.log(addition);
+console.log(subtraction);
+console.log(division);
+console.log(multiplication);
